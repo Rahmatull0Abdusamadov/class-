412a86daed6f8828b6f0e29e2a4f04bd0ed2b206
Rahmatullo <rahmatulloabdusamadov167@gmail.com>
2024-07-26 09:25:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+// class task
+// function task(a) {
+//     if (a.length>0){
+//       return a[0]; 
+//     } else {
+//       return undefined; 
+//     }
+//   }
+//   console.log(task([1, 2, 3]));   
+  
+// task2
+// function task(...a) {
+//     return a;
+//   }
+//   console.log(task(1, 2));       
+  
+// task3
+// function task(a) {
+//     return a.reverse();
+//   }
+//   console.log(task([1, 2, 3, 4]));    
+// task5
+// function task(a) {
+//     if (a.length>0) {
+//       return a[a.length - 1]; 
+//     } else {
+//       return undefined; 
+//     }
+//   }
+//   console.log(task([1, 2, 3]));       
+  
+// // task6
+// function task(a) {
+//     return a.join('');
+//   }
+//   console.log(task([1, 2, 3, 4, 5, 6]));    
+
+// task7
+// function task(a,b) {
+//     return a.includes(b);
+//   }
+//   console.log(task([1, 2, 3, 4, 5], 3));
+
+// task8
+// function task(a,b) {
+//     return a.indexOf(b);
+//   }
+//   console.log(task(["hi", "edabit", "fgh", "abc"], "fgh"));
+  
+// task9
+// function task(a,b) {
+//     return a.concat(b);
+//   }
+//   console.log(task([1, 3, 5], [2, 6, 8]));     
+  
+  
+  
+  
